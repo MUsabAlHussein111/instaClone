02b110969fe636755949e872b61c337b4b8f116b
mix11developer <65042111+mix11developer@users.noreply.github.com>
2023-12-18 21:50:21 +0300

    Add files via upload

---
+++ PortalDemo.js
@@ -0,0 +1,9 @@
+import { createPortal } from "react-dom"
+function PortalDemo() {
+  return createPortal(
+    <div>PortalDemo</div>,
+    document.getElementById("portal-root")
+  )
+}
+
+export default PortalDemo
